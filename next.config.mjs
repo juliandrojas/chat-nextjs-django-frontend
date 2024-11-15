8edabc01742890e3afeb4b127a051c9b4515bc5e
Julian Rojas <juliandra140201@gmail.com>
2024-11-15 12:13:01 -0500

    Add("Local and Vercel API_URL")

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     env: {
         API_URL: "https://chat-backend-clase-produccion.vercel.app"
+        //API_URL: "http://localhost:8000/api/chat/" // Use this for local development
     }
 };
 
