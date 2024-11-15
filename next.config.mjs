78a83c009ee3dc630db120d2c6ec6dd7dcd8f403
Julian Rojas <juliandra140201@gmail.com>
2024-11-15 12:23:16 -0500

    Fix("Direction of API URL")

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        API_URL: "https://chat-backend-clase-produccion.vercel.app"
+        API_URL: "https://chat-backend-clase-produccion.vercel.app/api/chat/"
         //API_URL: "http://localhost:8000/api/chat/" // Use this for local development
     }
 };
